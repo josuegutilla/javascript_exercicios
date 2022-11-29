df565dc36e67b0241962f4a48211448ef1abf1e5
josuegutilla <105397445+josuegutilla@users.noreply.github.com>
2022-11-29 16:41:49 -0400

    métodos numéricos

---
+++ 17 _let_e_const/js/scripts.js
@@ -0,0 +1,34 @@
+let x = 10;  //var x = 10; 
+x = 20; 
+console.log(x); 
+
+
+const y = 40; 
+console.log('const 1 ' +  y);
+
+
+
+if(true) {
+
+    let x = 25; 
+    console.log('let if ' +  x);
+
+    const y = 50; 
+    console.log('const 2 ' + y)
+
+}
+
+
+console.log(x);
+
+
+if(20 > 10){
+    const y = 69;
+    console.log('const 3 ' + y);
+}
+
+for(let x = 0; x < 10; x++){
+    console.log('let for ' + x);
+}
+
+console.log('Olá Mundo ');
---
+++ 18_metodos_numericos/js/scripts.js
@@ -0,0 +1,28 @@
+// pareseFloat 10.0
+
+console.log(parseFloat('12.999'));   //tranfoma em NUMBER.
+console.log(Number.parseFloat('12.999'));   //tranfoma em NUMBER.
+
+
+// parseInt
+
+console.log(parseInt('10'));   //tranforma em INTEIRO.
+console.log(parseInt(15.98797));  //tranforma em INTEIRO.
+
+
+//toFixed
+
+console.log(23.2983912748.toFixed(1))   //coloca quantas casas q quiser.
+
+
+//isNan
+
+console.log(isNaN('teste'));  //para verificar se é uma string (true/false).
+console.log(isNaN(18));   //para verificar se é uma string (true/false).
+console.log(isNaN('18'));   //para verificar se é uma string (true/false).
+
+
+//MAX_VALUE E MIN_VALUE
+
+console.log(Number.MAX_VALUE);  //valor maximo de número no js.
+console.log(Number.MIN_VALUE);  //valor minimo de númeor no js.
