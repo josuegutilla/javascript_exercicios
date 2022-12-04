8b517f08f3214a7b64b3cacb0be2305a0fd4505c
josuegutilla <105397445+josuegutilla@users.noreply.github.com>
2022-12-04 17:59:16 -0400

    selecionando elemento e trocando conteúdo

---
+++ 26_alterando_conteudo/js/scripts.js
@@ -0,0 +1,11 @@
+// selecionar elemento 
+
+var title = document.querySelector('#titulo');
+
+// innerHTML
+title.innerHTML = 'Olá meu nome é Josué';
+
+
+//textContent -> mais recomendado, + utilizado e perfomatico; 
+var subtitle = document.querySelector('.subtitulo');
+subtitle.textContent = 'Alterando o segundo texto';
